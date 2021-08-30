00fad55f05c7a62c3c396eb3a8698fa5e1858e56
s-haythem <haythem.smirani@gmail.com>
2021-08-30 15:56:04 +0100

    com

--- src/Components/Rate.js
+++ src/Components/Rate.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import StarIcon from '@material-ui/icons/Star';
+import React from "react";
+import StarIcon from "@material-ui/icons/Star";
 
+const Rate = ({ setRating }) => {
+  let arr = Array(5).fill(<StarIcon style={{ color: "yellow" }} />);
 
-const Rate = ({setRating })=>{
-    let arr = Array(5).fill(<StarIcon style={{color:'yellow'}}/>)
-    
-    return (
-        <div className="Rate" >
-            
-        {arr.map((el,i)=> <button onClick={()=>setRating(i)} className= "star-btn">{el}</button>)}
-            
-        </div>
-    )}
+  return (
+    <div className="Rate">
+      {arr.map((el, i) => (
+        <button onClick={() => setRating(i+1)} className="star-btn">
+          {el}
+        </button>
+      ))}
+    </div>
+  );
+};
 
-export default Rate 
-
-
+export default Rate;
